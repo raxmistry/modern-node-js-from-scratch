aad5620132b28c7d0ed8baa7439574258390311d
Rakesh Mistry <rakesh.mistry@gmail.com>
2020-03-17 21:37:27 +0200

    feat: basic http routing

--- my_try/index.js
+++ my_try/index.js
@@ -1,8 +1,24 @@
 const http = require('http')
+const fs = require('fs')
+
+const aboutPage = fs.readFile('about.html')
+const contactPage = fs.readFile('contact.html')
+const homePage = fs.readFile('index.html')
 
 const server = http.createServer((request, response) => {
     console.log(request.url)
-    response.end('HELLO NODE JS')
+
+    if (request.url === '/') {
+        response.end(homePage)
+    } else if (request.url === '/about') {
+        response.end(aboutPage)
+    } else if (request.url === '/contact') {
+        response.end(contactPage)
+    } else {
+        response.writeHead(404)
+        response.end("Page not found")
+    }
+    
 })
 
 server.listen(3000)
