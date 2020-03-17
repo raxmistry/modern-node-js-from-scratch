0c34b0f5af3843cc4125f20ee6a5a7c86a14a3bc
Rakesh Mistry <rakesh.mistry@gmail.com>
2020-03-17 22:11:33 +0200

    feat: load static resources with express

--- my_try/index.js
+++ my_try/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const app = new express()
+app.use(express.static('public'))
  
 app.get('/', (request, response) => { 
     response.sendFile(path.resolve(__dirname, 'index.html'))
