89fab06aaa3baf9efd5a433a3fae444371ad788f
Rakesh Mistry <rakesh.mistry@investec.co.za>
2020-03-31 15:06:55 +0200

    feat: encrypt password in db

--- nodejs-blog/database/models/User.js
+++ nodejs-blog/database/models/User.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt')
 const UserSchema = new mongoose.Schema( {  
 
     username: String,
     email: String,
     password: String
+})
+
+UserSchema.pre('save', function(next) {
+    const user = this
+    bcrypt.hash(user.password, 10, function(error, encrypted) {
+        user.password = encrypted
+        next()
+    })
 })
 
 module.exports = mongoose.model('User', UserSchema)
