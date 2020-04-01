531a6695091704fbd06d7185a1a265ffbe19308b
Rakesh Mistry <rakesh.mistry@gmail.com>
2020-04-01 22:03:58 +0200

    feat: wysiwyg editor for blog post

--- nodejs-blog/middleware/storePost.js
+++ nodejs-blog/middleware/storePost.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
 
-    if (!req.files || !req.files.image || !req.body.username ||
+    if (!req.files || !req.files.image || 
     !req.body.title || !req.body.subtitle || !req.body.content) {
         return res.redirect('/posts/new')
     }
